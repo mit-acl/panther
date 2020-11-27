a9671105db6f6a50b20b53134644b04bf13f49fc
Jesus Tordesillas <jtorde@mit.edu>
2020-11-27 00:16:17 -0500

    fixed bug transpose

--- toRotMat.m
+++ toRotMat.m
@@ -1,7 +1,7 @@
 %q should be expressed as [real i j k]
-%q is assummed to be normalized
-%Note that this function returns the transpose of quat2rotm(q)
-function R= toRotMat(q);
+%q is assummed to be normalized!!!
+%Note that this function should return the same as quat2rotm(q)
+function R= toRotMat(q)
 
 q=q(:);
 
@@ -10,9 +10,9 @@ q1=q(2);
 q2=q(3);
 q3=q(4);
 
-R=[q0^2+q1^2-q2^2-q3^2  2*q1*q2+2*q0*q3  2*q1*q3-2*q0*q2;
-    2*q1*q2-2*q0*q3     q0^2-q1^2+q2^2-q3^2  2*q2*q3+2*q0*q1;
-    2*q1*q3+2*q0*q2   2*q2*q3-2*q0*q1  q0^2-q1^2-q2^2+q3^2  
+R=[q0^2+q1^2-q2^2-q3^2  2*q1*q2-2*q0*q3  2*q1*q3+2*q0*q2;
+    2*q1*q2+2*q0*q3     q0^2-q1^2+q2^2-q3^2  2*q2*q3-2*q0*q1;
+    2*q1*q3-2*q0*q2   2*q2*q3+2*q0*q1  q0^2-q1^2-q2^2+q3^2  
     ];
 
 
