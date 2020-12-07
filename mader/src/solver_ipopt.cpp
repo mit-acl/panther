a39d3fff0f9d6cb3894697d3c209180d34d3f377
Jesus Tordesillas <jtorde@mit.edu>
2020-12-07 09:58:34 -0500

    formatting

--- mader/src/solver_ipopt.cpp
+++ mader/src/solver_ipopt.cpp
@@ -1,4 +1,5 @@
-// Taken (and modified) from https://github.com/casadi/casadi/blob/752695adf24e0d4c904cc0bbeeedbcb73025559d/docs/examples/cplusplus/cmake_pkgconfig/casadi_demo.cpp
+// Taken (and modified) from
+// https://github.com/casadi/casadi/blob/752695adf24e0d4c904cc0bbeeedbcb73025559d/docs/examples/cplusplus/cmake_pkgconfig/casadi_demo.cpp
 
 // Run first the casadi_demo.m file to generate the file my_function.casadi
 
@@ -11,36 +12,35 @@
 using namespace casadi;
 using namespace std;
 
-int main() {
+int main()
+{
   // Definitions of DM, DMVector,... are in dm_fwd.hpp
 
   // ///////////////////////// EXAMPLE OF HOW TO CALL a .casadi file
-  Function tmp = Function::load("./my_function.casadi");
-//   std::vector<DM> result =
-//       tmp(std::vector<DM>{3, std::vector<double>{0.1, 0.2, 0}});
+  Function tmp = Function::load("./../matlab/my_function.casadi");
+  //   std::vector<DM> result =
+  //       tmp(std::vector<DM>{3, std::vector<double>{0.1, 0.2, 0}});
 
-  
-  //Conversion DM <--> Eigen:  https://github.com/casadi/casadi/issues/2563
-  
-  
+  // Conversion DM <--> Eigen:  https://github.com/casadi/casadi/issues/2563
+
   std::map<std::string, DM> map_arguments;
-  map_arguments["theta_FOV_deg"]=80.0;
-  map_arguments["p0"]=std::vector<double>{-4.0, 0.0, 0};
-  map_arguments["v0"]=std::vector<double>{0.0, 0.0, 0};
-  map_arguments["a0"]=std::vector<double>{0.0, 0.0, 0};
-  map_arguments["pf"]=std::vector<double>{4.0, 0.0, 0};
-  map_arguments["vf"]=std::vector<double>{0.0, 0.0, 0};
-  map_arguments["af"]=std::vector<double>{0.0, 0.0, 0};
-  map_arguments["y0"]=0.0;
-  map_arguments["ydot0"]=0.0;
-  map_arguments["all_w_fe"]=DM::ones(3,15);
-  map_arguments["c_jerk"]=0.0;
-  map_arguments["c_yaw"]=0.0;
-  map_arguments["c_vel_isInFOV"]=1.0;
-  map_arguments["all_nd"]=DM::rand(4,0);
-  
-  std::map<std::string, DM> result =tmp(map_arguments);
-  
+  map_arguments["theta_FOV_deg"] = 80.0;
+  map_arguments["p0"] = std::vector<double>{ -4.0, 0.0, 0 };
+  map_arguments["v0"] = std::vector<double>{ 0.0, 0.0, 0 };
+  map_arguments["a0"] = std::vector<double>{ 0.0, 0.0, 0 };
+  map_arguments["pf"] = std::vector<double>{ 4.0, 0.0, 0 };
+  map_arguments["vf"] = std::vector<double>{ 0.0, 0.0, 0 };
+  map_arguments["af"] = std::vector<double>{ 0.0, 0.0, 0 };
+  map_arguments["y0"] = 0.0;
+  map_arguments["ydot0"] = 0.0;
+  map_arguments["all_w_fe"] = DM::ones(3, 15);
+  map_arguments["c_jerk"] = 0.0;
+  map_arguments["c_yaw"] = 0.0;
+  map_arguments["c_vel_isInFOV"] = 1.0;
+  map_arguments["all_nd"] = DM::rand(4, 0);
+
+  std::map<std::string, DM> result = tmp(map_arguments);
+
   std::cout << result << std::endl;
   //////////////////////////
 
