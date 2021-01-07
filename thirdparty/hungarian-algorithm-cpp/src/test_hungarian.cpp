cc2323e8d5244f73f231bdbcbe271ac24105b4ff
Jesus Tordesillas <jtorde@mit.edu>
2021-01-07 11:45:49 -0500

    minor

--- thirdparty/hungarian-algorithm-cpp/src/test_hungarian.cpp
+++ thirdparty/hungarian-algorithm-cpp/src/test_hungarian.cpp
@@ -5,9 +5,9 @@
 int main(void)
 {
   // please use "-std=c++11" for this initialization of vector.
-  vector<vector<double> > costMatrix = {
-    { 10, 19, 8, 15, 0 }, { 10, 18, 7, 17, 0 }, { 13, 16, 9, 14, 0 }, { 12, 19, 8, 18, 0 }
-  };
+  vector<vector<double> > costMatrix = { { 10, 19, 8, 15, 0 }, { 10, 18, 7, 17, 0 }, { 13, 16, 9, 14, 0 },
+                                         { 12, 19, 8, 18, 0 }, { 12, 19, 8, 18, 0 }, { 12, 19, 8, 18, 0 },
+                                         { 10, 19, 8, 15, 0 } };
 
   HungarianAlgorithm HungAlgo;
   vector<int> assignment;
