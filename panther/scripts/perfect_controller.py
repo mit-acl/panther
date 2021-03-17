af0e26e6d80dcbf195b5987b168e58ba040770cb
Jesus Tordesillas <jtorde@mit.edu>
2021-03-16 20:14:56 -0400

    added () in print

--- panther/scripts/perfect_controller.py
+++ panther/scripts/perfect_controller.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
             rospy.logfatal("This is tyipcally accomplished in a launch file.")
             rospy.logfatal("Command line: ROS_NAMESPACE=mQ01 $ rosrun quad_control joy.py")
         else:
-            print "Starting perfect tracker node for: " + ns
+            print ("Starting perfect tracker node for: " + ns)
             startNode()
     except rospy.ROSInterruptException:
         pass
