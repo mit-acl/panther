8601b0b9f983950e9f4ee7bf1f638e2616adfdaa
Jesus Tordesillas <jtorde@mit.edu>
2021-05-07 08:02:01 -0400

    progress

--- panther/matlab/using_fitting_manifolds.m
+++ panther/matlab/using_fitting_manifolds.m
@@ -13,7 +13,7 @@ addpath(genpath('./more_utils'));
 addpath(genpath('./hopf_visualization'));
 
 set(0,'DefaultFigureWindowStyle','docked') %'normal' 'docked'
-
+set(0,'defaultfigurecolor',[1 1 1])
 opti = casadi.Opti();
 
 
@@ -25,7 +25,7 @@ num_max_of_obst=10; %This is the maximum num of the obstacles
 basis="MINVO"; %MINVO OR B_SPLINE or BEZIER. This is the basis used for collision checking (in position, velocity, accel and jerk space), both in Matlab and in C++
 
 t0=0; 
-tf=10.5;
+tf=8.5;
 
 dim_pos=3;
 dim_yaw=1;
@@ -33,9 +33,9 @@ dim_yaw=1;
 sp=MyClampedUniformSpline(t0,tf,deg_pos, dim_pos, num_seg, opti); %spline position.
 sy=MyClampedUniformSpline(t0,tf,deg_yaw, dim_yaw, num_seg, opti); %spline yaw.
 
-pCPs=[   -4.0000   -4.0000   4.0000    0.7111    3    3   4;
-         -2         0         0   5   -0.0131   2   -0.0131;
-         0         2         -2    0.6275    0.0052    3    0.0052];
+pCPs=[   -4.0000   -4.0000   8    5    3   2     -4;
+         -2         0         0   -5   -7   -8   -9;
+         0         2         -2    7    0.0052    3    0.0052];
      
 sp.setCPoints(mat2cell(pCPs,[3],[ones(1,size(pCPs,2))]))
 
@@ -49,15 +49,17 @@ my_colormap=autumn;
 b_T_c= [roty(90)*rotz(-90) zeros(3,1); zeros(1,3) 1];
 w_fevar=[1 -2 3]';
 
-for t=t0:0.6:tf
+all_t=t0:0.6:tf;
+
+for t=all_t
     
     pos=sp.getPosT(t);
     a=sp.getAccelT(t);
+    ee=w_fevar-pos;
     xi=a+[0 0 9.81]';
     tmp=pos+xi/norm(xi);
     
     arrow3d(pos',tmp',20,'cylinder',[0.2,0.1]);
-    
     qabc=qabcFromAccel(a, 9.81);
     
     
@@ -67,11 +69,8 @@ for t=t0:0.6:tf
     
     n_psi_samples=30;
     all_psi=linspace(0,2*pi,n_psi_samples);
-    
-%     my_map=phasemap(n_psi_samples);
-    
+        
     my_map=autumn;
-    my_map=[my_map];%; flip(my_map)]
        
     all_values=[];
     all_projs=[];
@@ -82,56 +81,89 @@ for t=t0:0.6:tf
         R=toRotMat(q);
         
         b0=R(:,1)*norm(a);
-        ee=w_fevar-pos;
+        
         value=norm(ee)*cosd(80)*norm(a)-b0'*ee; %This is the cost (the more negative it is--> the more in the FOV it is)
         all_values=[all_values value];
 
         all_projs=[all_projs R(:,1)+pos]; %Assumming here that
-%          c_T_b=invPose(b_T_c);
-%          b_T_w=invPose(w_T_b);
-%     
-%          c_P=c_T_b*b_T_w*[w_fevar;1]; %Position of the feature in the camera frame
-%          s=c_P(1:2)/(c_P(3));  %Note that here we are not using f (the focal length in meters) because it will simply add a constant factor in ||s|| and in ||s_dot||
-%          
-%          norm(s)
-%         
+       
     end
     
-%     all_values=all_projs(1,:);
-    
     all_values= (all_values - min(all_values)) / ( max(all_values) - min(all_values) ); %normalize \in [0,1]
-    
     n=size(my_map,1);
     all_values=  ceil((n-1)*all_values + 1); %normalize \in [1,n]
     colors=my_map(all_values',:);
-%     for i=1:numel(all_psi)
-%          index=ceil(all_values*numel(my_phasemap))+1;
-%          index=max(index,numel(my_phasemap));
-%          colors=[colors my_phasemap(index)]
-%     end
+
 
 h=colormapline(all_projs(1,:),all_projs(2,:),all_projs(3,:),colors); axis equal;
 set(h,'linewidth',4,'linestyle','--')
     
-    
 end
 
 plotSphere(w_fevar,0.2,'g');
+p1=[w_fevar(1:2);0];
+p2=w_fevar;
+plot3([ p1(1) p2(1)], [p1(2) p2(2)], [p1(3) p2(3)],'--');
+% plot([w_fevar(1:2);0],w_fevar,'--' )
 
+camlight
+
+angles_datapoints=[];
+dataPoints={};
+for t=all_t
+    
+    pos=sp.getPosT(t);
+    a=sp.getAccelT(t);
+    ee=w_fevar-pos;
+    xi=a+[0 0 9.81]';
+
+    r0_star=(ee*norm(xi)^2 - (ee'*xi)*xi);
+    r0_star=r0_star/norm(r0_star);
+    
+%     r3=xi/norm(xi);
+%     r2=cross(r3,r0_star);
+    
+    tmp=pos+r0_star;
+    arrow3dWithColor(pos',tmp',30,'cylinder',[0.2,0.1],'b');
+
+    qabc=qabcFromAccel(a, 9.81);
+    Rabc=toRotMat(qabc);
+    Tabc=[Rabc pos; 0 0 0 1];
+    
+    b_r0star=inv(Tabc)*[tmp;1];
+    b_r0star=b_r0star(1:3);
+    assert(abs(b_r0star(3))<0.0001)
+    
+    dataPoints{end+1}=b_r0star(1:2)';
+    angles_datapoints=[angles_datapoints atan2(b_r0star(2),b_r0star(1))];
+%     
+%     u=r0_star; v=Rabc(:,1);
+%     CosTheta = max(min(dot(u,v)/(norm(u)*norm(v)),1),-1);
+%     angles=[angles real(acos(CosTheta))];    
+%     
+%     dataPoints=[dataPoints  (u'*v)*v u-(u'*v)*v];
+%     
+%     
+%     w_T_b=[toRotMat(qabc) pos; 0 0 0 1];
+    
+end
+
+dataPoints=dataPoints';
 
 
 %%
 
 
-n_d=100; %number of datapoints
-f = @(phi) [cos(phi) , sin(phi)];
-phi     = linspace(0,2*pi-0.5,n_d)'+2*rand(1,n_d)';
-dataPoints = mat2cell(f(phi),ones(1,length(phi)),2);
+% n_d=100; %number of datapoints
+% f = @(phi) [cos(phi) , sin(phi)];
+% angles_datapoints     = linspace(0,2*pi-0.5,n_d)'+2*rand(1,n_d)';
+% dataPoints = mat2cell(f(phi),ones(1,length(phi)),2);
+
 nData   = length(dataPoints);
 dataCoords = linspace(0,1,nData);
 M = spherefactory(2);
 t = linspace(0,1,(nData-1)*30);
-lambda = 3; % fitting
+lambda = 300; % fitting
 S = blendedCurve(M,dataPoints,dataCoords,t,'lambda',lambda);
 
 figure;
@@ -147,4 +179,5 @@ end
 % end
 
 plot(t,angles,'-o'); hold on;
-plot(dataCoords,phi,'o')
+scatter(dataCoords,angles_datapoints,60,'o','filled')
+yline(pi,'--'); yline(-pi,'--'); xlabel('t'); ylabel('psi');
